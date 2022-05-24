523a9d8a5c6b2ea6271b857c6bac2aa16c81e2cc
tarikceylan <tarikceylan@outlook.com>
2022-05-25 01:22:26 +0300

    Loaders Intro Implemented

--- src/app/utils/get-data.js
+++ src/app/utils/get-data.js
@@ -1,11 +1,11 @@
-export const cb = (data) => {
-    $('.hobbies').click(() => {
-        _.forEach( data.hobbies, (hobby) => {
-          if($('.hobbies-list > li').length < data.hobbies.length) {
-            $('.hobbies-list').append(`<li class="hobbies-list-item list-group-item"> ${hobby}`);
-          } else  {
-            $('.hobbies-list').toggleClass('no-show');
-          }
-        });
+const cb = (data) => {
+  $('.hobbies').click(() => {
+      _.forEach( data.hobbies, (hobby) => {
+        if($('.hobbies-list > li').length < data.hobbies.length) {
+          $('.hobbies-list').append(`<li class="hobbies-list-item list-group-item"> ${hobby}`);
+        } else  {
+          $('.hobbies-list').toggleClass('no-show');
+        }
       });
-    }
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {ouch} from "./app/utils/ouch";
 import {cb} from "./app/utils/get-data";
+import "./main.css";
 ouch();
 cb();
