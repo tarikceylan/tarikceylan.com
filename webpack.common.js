56cbec360ca773d7e22b5a9506adf86429647622
tarikceylan <tarikceylan@outlook.com>
2022-05-26 21:29:31 +0300

    Seperate Prod and Dev Env

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,8 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "[name].[hash].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [new HtmlWebpackPlugin({
     template: "./src/template.html",
     inject: "body"
